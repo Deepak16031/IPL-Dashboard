df0dbfb9c9d4e50c791441550a9c523b86f920c1
deepak <deepak16031@iiitd.ac.in>
2022-04-29 13:20:04 +0530

    sample commit

--- src/main/java/com/deepak/ipldashboard/IpldashboardApplication.java
+++ src/main/java/com/deepak/ipldashboard/IpldashboardApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IpldashboardApplication {
 
+	// comments for git testing
 	public static void main(String[] args) {
 		SpringApplication.run(IpldashboardApplication.class, args);
 	}
